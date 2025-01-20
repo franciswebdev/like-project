a3cab920eaeee48acc721aa4565ea06942a02ccf
Francis del Mundo <francisd@sbs.com.au>
2025-01-20 11:46:56 +1100

    Refactored

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,135 +1,15 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Logger,
-  NotFoundException,
-  Param,
-  Post,
-  Response,
-} from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Response } from '@nestjs/common';
 import { AppService } from './app.service';
-import {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} from '@aws-sdk/lib-dynamodb';
-import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
-  private readonly LIKES_TABLE = process.env.LIKES_TABLE;
-  private readonly USERS_TABLE = process.env.USERS_TABLE;
-  private readonly client: DynamoDBClient;
-  private readonly docClient: DynamoDBDocumentClient;
 
-  constructor(private readonly appService: AppService) {
-    this.client = new DynamoDBClient();
-    this.docClient = DynamoDBDocumentClient.from(this.client);
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/like/:userId')
-  async getUserSongs(@Param('userId') userId, @Response() res) {
-    const user = await this.getUserAndSongs(userId);
-    res.json({ user });
-  }
-
-  @Get('/like/:userId/:songId')
-  async getUserSong(
-    @Param('userId') userId,
-    @Param('songId') songId,
-    @Response() res,
-  ) {
-    const user = await this.getUserAndSongs(userId, songId);
-    res.json({ user });
-  }
-
-  private async getUserAndSongs(userId: string, songId?: string) {
-    let res = null;
-
-    try {
-      const command = new QueryCommand({
-        TableName: this.USERS_TABLE,
-        KeyConditionExpression: '#user_id = :user_id_value',
-        ExpressionAttributeNames: {
-          '#user_id': 'userId',
-        },
-        ExpressionAttributeValues: {
-          ':user_id_value': { S: userId },
-        },
-      });
-      const { Items, Count } = await this.docClient.send(command);
-      this.logger.log(`Count of items = ${Count} for ${userId} and ${songId}`);
-      if (Count > 0) {
-        res = songId ? Items.find((item) => item.songId.S === songId) : Items;
-      } else {
-        this.logger.error(`No items found for ${userId}`);
-      }
-    } catch (error) {
-      throw new NotFoundException(
-        `Error on ${this.USERS_TABLE}: ${error.message}`,
-      );
-    }
-
-    return res;
-  }
-
-  private async getSongLikes(songId: string) {
-    let res = null;
-
-    try {
-      const params = {
-        TableName: this.LIKES_TABLE,
-        Key: {
-          songId,
-        },
-      };
-      this.logger.log(`Table ${params.TableName} and Key ${params.Key} --`);
-      const command = new GetCommand(params);
-      const { Item } = await this.docClient.send(command);
-      if (Item) {
-        const { songId, likes } = Item;
-        res = { songId, likes };
-      }
-    } catch (error) {
-      throw new NotFoundException(
-        `Error on ${this.LIKES_TABLE}: ${error.message}`,
-      );
-    }
-
-    return res;
-  }
-
-  private async getUser(userId: string): Promise<{ userId: string; songs: string[] }> {
-    let res = null;
-
-    try {
-      const params = {
-        TableName: this.USERS_TABLE,
-        Key: {
-          userId,
-        },
-      };
-      this.logger.log(`Table ${params.TableName} and Key ${params.Key} --`);
-      const command = new GetCommand(params);
-      const { Item } = await this.docClient.send(command);
-      if (Item) {
-        const { userId, songs } = Item;
-        res = { userId, songs };
-      }
-    } catch (error) {
-      throw new NotFoundException(
-        `Error on ${this.USERS_TABLE}: ${error.message}`,
-      );
-    }
-
-    return res;
   }
 
   @Post('/like')
@@ -143,44 +23,47 @@ export class AppController {
       // - option 1 is using a device id that may get disabled if user disables tracking
       // - option 2 is mac address but is also not ideal
       // - option 3 or default is anything generated perhaps the timestamp the app is installed
-
-      let userData = await this.getUser(userId);
+      let userData = await this.appService.getUser(userId);
+      let songFound = false;
       if (!userData) {
         this.logger.log(`User not found, creating one for ${userId}`);
         userData = { userId, songs: [songId] };
       } else {
         this.logger.log(`User found, updating for ${userId}`);
-        const foundSong = userData.songs.find(song => song === songId );
-        if (!foundSong) userData.songs.push(songId);
+        const foundSong = userData.songs.find((song) => song === songId);
+        if (!foundSong) {
+          this.logger.log(`Adding song ${songId}`);
+          userData.songs.push(songId);
+        } else {
+          this.logger.log(`Removing song ${songId}`);
+          userData.songs = userData.songs.filter((song) => song !== songId);
+          songFound = true;
+        }
       }
 
-      const usersParams = {
-        TableName: this.USERS_TABLE,
-        Item: userData,
-      };
-      postPromises.push(this.docClient.send(new PutCommand(usersParams)));
+      // Save changes to user!
+      postPromises.push(this.appService.updateUser(userData));
 
-      // likes table is for saving all likes from users for a song, users are not saved
-      // this is an INDEX table
-      let likesData = await this.getSongLikes(songId);
+      // likes table is for saving all likes from users for a song ONLY
+      let likesData = await this.appService.getSongLikes(songId);
       if (!likesData) {
         this.logger.log(`Song not found, creating one for ${songId}`);
         likesData = { songId, likes: 1 };
       } else {
         this.logger.log(`Song found, updating for ${songId}`);
-        likesData = { songId, likes: likesData.likes + 1 };
+        likesData = {
+          songId,
+          likes: likesData.likes + (!songFound ? 1 : -1),
+        };
       }
 
-      const likesParams = {
-        TableName: this.LIKES_TABLE,
-        Item: likesData,
-      };
-      postPromises.push(this.docClient.send(new PutCommand(likesParams)));
+      // Save changes if any to liked songs!
+      postPromises.push(this.appService.updateSongLikes(likesData));
 
       const settled = await Promise.allSettled(postPromises);
-      res.json({ userId, user: userData, results: settled });
+      res.json({ userData, likesData, results: settled });
     } catch (error) {
-      console.error(error);
+      this.logger.error(error);
       res.status(500).json({ error: 'Could not create user' });
     }
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  private readonly LIKES_TABLE = process.env.LIKES_TABLE;
+  private readonly USERS_TABLE = process.env.USERS_TABLE;
+  private readonly client: DynamoDBClient;
+  private readonly docClient: DynamoDBDocumentClient;
+
+  constructor() {
+    this.client = new DynamoDBClient();
+    this.docClient = DynamoDBDocumentClient.from(this.client);
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Like project is OK!';
+  }
+
+  public async getSongLikes(songId: string) {
+    let res = null;
+
+    try {
+      const params = {
+        TableName: this.LIKES_TABLE,
+        Key: {
+          songId,
+        },
+      };
+      this.logger.log(`Table ${params.TableName} and Key ${params.Key} --`);
+      const command = new GetCommand(params);
+      const { Item } = await this.docClient.send(command);
+      if (Item) {
+        const { songId, likes } = Item;
+        res = { songId, likes };
+      }
+    } catch (error) {
+      throw new NotFoundException(
+        `Error on ${this.LIKES_TABLE}: ${error.message}`,
+      );
+    }
+
+    return res;
+  }
+
+  public async getUser(
+    userId: string,
+  ): Promise<{ userId: string; songs: string[] }> {
+    let res = null;
+
+    try {
+      const params = {
+        TableName: this.USERS_TABLE,
+        Key: {
+          userId,
+        },
+      };
+      this.logger.log(`Table ${params.TableName} and Key ${params.Key} --`);
+      const command = new GetCommand(params);
+      const { Item } = await this.docClient.send(command);
+      if (Item) {
+        const { userId, songs } = Item;
+        res = { userId, songs };
+      }
+    } catch (error) {
+      throw new NotFoundException(
+        `Error on ${this.USERS_TABLE}: ${error.message}`,
+      );
+    }
+
+    return res;
+  }
+
+  public updateUser(userData) {
+    const usersParams = {
+      TableName: this.USERS_TABLE,
+      Item: userData,
+    };
+    
+    return this.docClient.send(new PutCommand(usersParams));
+  }
+
+  public updateSongLikes(likesData) {
+    const likesParams = {
+      TableName: this.LIKES_TABLE,
+      Item: likesData,
+    };
+    return this.docClient.send(new PutCommand(likesParams));
   }
 }
